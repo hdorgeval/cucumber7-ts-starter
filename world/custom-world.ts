c21e68bce8d6b04aea18a865c223bfdf3160e7ee
hdorgeval <henri.dorgeval@outlook.fr>
2021-01-24 22:10:05 +0100

    fix(custom-world): fix typings for the attach function

--- world/custom-world.ts
+++ world/custom-world.ts
@@ -3,17 +3,19 @@ import { setWorldConstructor } from '@cucumber/cucumber';
 import { Stream } from 'stream';
 
 export type MediaType = 'text/plain' | 'image/png' | 'application/json';
-export type AttachBuffer = (data: Buffer, mediaType: MediaType) => void;
-export type AttachStream = (data: Stream, mediaType: MediaType) => void;
-export type AttachText = (data: string) => void;
-export type AttachStringifiedJson = (data: string, mediaType: 'application/json') => void;
-export type AttachBase64EncodedPng = (data: string, mediaType: 'image/png') => void;
-export type AttachFn =
-  | AttachBuffer
-  | AttachStream
-  | AttachBase64EncodedPng
-  | AttachStringifiedJson
-  | AttachText;
+export type AttachBuffer = (data: Buffer, mediaType: MediaType) => void | Promise<void>;
+export type AttachStream = (data: Stream, mediaType: MediaType) => void | Promise<void>;
+export type AttachText = (data: string) => void | Promise<void>;
+export type AttachStringifiedJson = (
+  data: string,
+  mediaType: 'application/json',
+) => void | Promise<void>;
+export type AttachBase64EncodedPng = (data: string, mediaType: 'image/png') => void | Promise<void>;
+export type AttachFn = AttachBuffer &
+  AttachStream &
+  AttachBase64EncodedPng &
+  AttachStringifiedJson &
+  AttachText;
 
 export interface CucumberWorldConstructorParams {
   attach: AttachFn;
