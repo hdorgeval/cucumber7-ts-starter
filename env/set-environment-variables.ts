f269212702409a75dca9c996d25fdee13059ef74
hdorgeval <henri.dorgeval@outlook.fr>
2021-01-25 13:53:04 +0100

    chore(ci): disable chalk colors on CI

--- env/set-environment-variables.ts
+++ env/set-environment-variables.ts
@@ -1,4 +1,12 @@
-process.env['--logLevel'] = 'error';
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const isCI = require('is-ci');
+
 process.env['--foo'] = 'bar';
-process.env['--no-color'] = 'true';
 process.env['SimpleConsoleFormatter.printEnvelopes'] = 'false';
+
+if (isCI) {
+  // make error messages displayed within the html report readable
+  // https://github.com/chalk/chalk#chalksupportscolor
+  // --no-color
+  process.env['FORCE_COLOR'] = '0';
+}
